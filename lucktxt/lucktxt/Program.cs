5f118cdafa10df041f0f226e4c45343219e64507
Emin tasci <Emin.tasci@elev.ga.ntig.se>
2023-08-30 10:44:00 +0200

    ytcjyctjycjy

---
+++ lucktxt/lucktxt/Program.cs
@@ -0,0 +1,37 @@
+﻿string name;
+string age;
+string color;
+
+
+Console.WriteLine("hej vad heter du?");
+
+name = Console.ReadLine();
+
+Console.WriteLine("Hur gammal är du?"); 
+
+age = Console.ReadLine (); 
+
+Console.WriteLine ("vad gillar du för färg");
+
+color = Console.ReadLine(); 
+
+Console.WriteLine( name + age + color); 
+
+int a;
+int.TryParse(age, out a);
+
+Console.ReadLine(); 
+
+if (a > 20)
+{
+  Console.WriteLine("Högre än fyra!");
+}
+
+if (name == "Micke")
+{
+  Console.WriteLine("Innehållet i variabeln name är samma som 'Micke'");
+
+  Console.ReadLine();
+
+  Console.WriteLine();
+}
---
+++ te22c.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, TE22!");
+
+string name = "Micke" + " bergström" 
+
+string name = Console.Readline();
+
+Console.WriteLine("Hej " + name + "! vad cool du är idag!");
+Console.WriteLine($"Hej Då {name}! vi ses imorgon!");
+
+Console.ReadLine();
